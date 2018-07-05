6fd80254b5f55f8a2486deddd04c6a26ac49a843
Jermaine Smikle <jermaine.smikle@aol.com>
2018-07-05 09:15:26 -0400

    first commit

---
+++ practice.js
@@ -0,0 +1,12 @@
+
+var x = document.getElementsByClassName("boxes");
+
+console.log(x);
+
+var i;
+
+for (i = 0; i < x.length; i++) {
+    x[i].style.backgroundColor = "white";
+}
+
+// javascript not working
